4790cffd4b52e0bb8b924ff572523de11d6f1312
Sevcan <sevcansarikaya8@gmail.com>
2025-04-28 02:38:01 +0300

    urun-detay sevcan

---
+++ urun-detay.js
@@ -0,0 +1,11 @@
+function changeImage(src) {
+    document.getElementById('mainImage').src = src;
+  }
+
+  function buyNow() {
+    alert("Şimdi satın alma işlemine yönlendiriliyorsunuz!");
+  }
+
+  function addToCart() {
+    alert("Ürün sepete başarıyla eklendi!");
+  }
