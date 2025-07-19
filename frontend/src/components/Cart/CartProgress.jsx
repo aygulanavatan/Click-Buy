d02af6966ade467f4e6613ddf052f7796ffaea3c
melekyilmaz1 <meleky2307@gmail.com>
2025-07-19 17:27:16 +0300

    melek

--- frontend/src/components/Cart/CartProgress.jsx
+++ frontend/src/components/Cart/CartProgress.jsx
@@ -1,15 +1,14 @@
 const CartProgress = () => {
-    return (
-      <div className="free-progress-bar">
-        <p className="progress-bar-title">
-          Sepete <strong>800₺</strong> daha ekleyin, kargo ücretsiz olsun!
-        </p>
-        <div className="progress-bar">
-          <span className="progress"></span>
-        </div>
+  return (
+    <div className="free-progress-bar">
+      <p className="progress-bar-title">
+        Sepete <strong>800₺</strong> daha ekleyin, kargo ücretsiz olsun!
+      </p>
+      <div className="progress-bar">
+        <span className="progress"></span>
       </div>
-    );
-  };
-  
-  export default CartProgress;
-  
+    </div>
+  );
+};
+
+export default CartProgress;
